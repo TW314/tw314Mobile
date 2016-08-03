0dbb30fd56d21b6fddb4f20a5e7757198377527b
Antônio Carlos de Araújo Lima Filho <antonio.lima16@fatec.sp.gov.br>
2016-08-03 10:02:45 -0300

    Upando atualizações
    Tentando aplicar código da entrada de dados

--- tw314/app/src/main/java/com/example/voids/tw314/MainActivity.java
+++ tw314/app/src/main/java/com/example/voids/tw314/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.voids.tw314;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText entradaCodigo = (EditText) findViewById(R.id.entradaCod);
+
+        Button btnInserirCodigo = (Button) findViewById(R.id.btnInserirCodigo);
+
+        btnInserirCodigo.setOnClickListener(new Button.OnClickListener()
+            {
+                public void onClick(View v)
+                {
+
+                }
+            }
+        );
+
     }
 }
