24d07e8fe1827f39ea038126f564a65494b443b7
Pedro <pedromiguelscian@hotmail.com>
2016-08-11 17:08:17 -0300

    INIT: Commit inicial do projeto DONE: Tela inicial do aplicativo, Tela de Acesso

---
+++ app/src/main/java/tw314/tw314mobile/AcessoActivity.java
@@ -0,0 +1,13 @@
+package tw314.tw314mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcessoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acesso);
+    }
+}
