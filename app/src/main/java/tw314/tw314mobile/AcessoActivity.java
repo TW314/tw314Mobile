9a1c7eaf636ae07fdeaeff049c183177c6fe9d39
Pedro <pedromiguelscian@hotmail.com>
2016-08-16 18:44:27 -0300

    DONE: Padronização das nomenclaturas para inglês DONE: Separação das Strings por activity DONE: Tela Principal DOING; Tela de leitura Sobre

--- app/src/main/java/tw314/tw314mobile/AcessoActivity.java
+++ app/src/main/java/tw314/tw314mobile/AcessoActivity.java
@@ -8,6 +8,6 @@ public class AcessoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acesso);
+        setContentView(R.layout.activity_access);
     }
 }
