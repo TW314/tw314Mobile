820373e4f536e8b8b7f5bcee23683ef969ffbba8
Pedro <pedromiguelscian@hotmail.com>
2016-08-17 19:28:36 -0300

    DONE: Primeiro estado do projeto. TODO: Menu, FAQ, Settings

---
+++ app/src/main/java/tw314/tw314mobile/MainLayoutActivity.java
@@ -0,0 +1,16 @@
+package tw314.tw314mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Pedro on 17/08/2016.
+ */
+public class MainLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access);
+    }
+}
