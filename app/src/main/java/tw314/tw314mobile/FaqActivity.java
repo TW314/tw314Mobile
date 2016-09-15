a5e9e925196f884c0afdcd4d3e6922e2797737f2
pedromscian <pedromiguelscian@hotmail.com>
2016-09-15 00:13:29 -0300

    DONE: FaqActivity com AppCompatActivity DONE: Alteração do layout da AboutActivity DOING: Alteração do layout da AccessActivity DONE: String de logo adicionada ao strings.xmls

--- app/src/main/java/tw314/tw314mobile/FaqActivity.java
+++ app/src/main/java/tw314/tw314mobile/FaqActivity.java
@@ -2,7 +2,9 @@ package tw314.tw314mobile;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.Arrays;
@@ -11,13 +13,20 @@ import java.util.List;
 /**
  * Created by Pedro on 04/09/2016.
  */
-public class FaqActivity extends ListActivity {
+public class FaqActivity extends AppCompatActivity {
+
+    EditText edtSearch;
+    ListView listView;
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
 
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listQuestions()));
+        edtSearch = (EditText) findViewById(R.id.edtSearch);
+        listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listQuestions()));
     }
 
     private List<String> listQuestions() {
